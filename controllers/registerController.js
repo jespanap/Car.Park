7ec75ea1b5b45590505b55e27d99f519abe53bbe
jespanap1 <jespanap@eafit.edu.co>
2023-11-07 20:13:56 -0500

    using express-validator

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,6 +1,8 @@
 const userModel = require("../models/user.js");
 const bcrypt = require("bcrypt");
 const path = require("path");
+const { validationResult } = require("express-validator");
+const registerValidations = require("../middlewares/validations/registerValidations.js"); 
 
 const controller = {
   getRegister: (req, res) => {
@@ -8,23 +10,37 @@ const controller = {
     res.render('register');
   },
 
-  postUser: (req, res) => {
-    // Crear un objeto de usuario con los datos del formulario
-    const newUser = {
-      ...req.body
-    };
-    //hasehamos la contraseña
-    const newPassword = bcrypt.hashSync(newUser.password, 12);
+  postUser: [
+    // Utiliza las validaciones del archivo registerValidations
+    registerValidations.registerCheck,
 
-    newUser.password = newPassword;
+    (req, res) => {
+      // Verifica si hay errores de validación
+      const errors = validationResult(req);
 
-    // Llamar a la función para crear un nuevo usuario en el modelo
-    userModel.createOne(newUser);
+      if (!errors.isEmpty()) {
+        // Si hay errores, muestra una respuesta con los errores de validación
+        return res.render('register', { errors: errors.array() });
+      }
 
-    // Redirigir o responder al cliente según tus necesidades
-    // Puedes redirigir al usuario a una página de éxito o enviar una respuesta de éxito
-    res.send("Usuario registrado exitosamente");
-  },
+      // Crear un objeto de usuario con los datos del formulario
+      const newUser = {
+        ...req.body
+      };
+
+      // Haseheamos la contraseña
+      const newPassword = bcrypt.hashSync(newUser.password, 12);
+
+      newUser.password = newPassword;
+
+      // Llamar a la función para crear un nuevo usuario en el modelo
+      userModel.createOne(newUser);
+
+      // Redirigir o responder al cliente según tus necesidades
+      // Puedes redirigir al usuario a una página de éxito o enviar una respuesta de éxito
+      res.send("Usuario registrado exitosamente");
+    },
+  ],
 };
 
 module.exports = controller;
